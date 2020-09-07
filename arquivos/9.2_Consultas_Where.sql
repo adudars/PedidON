160aedd1d4a845c9fe6bcbc06b2c13aa8db133cb
Thiago <35338252+thiagocf2@users.noreply.github.com>
2020-09-07 10:46:40 -0300

    Add files via upload

---
+++ arquivos/9.2_Consultas_Where.sql
@@ -0,0 +1,7 @@
+select * from comanda where fk_id_funcionario=1;
+
+select id_comanda from comanda where mesa=1;
+
+select nome_cliente from cliente where id_cliente=10;
+
+select * from item_cardapio where preco<50;
